38b0c6427d26c53181b1e71237383fdf94ce9fa7
Andre Luis Kunert <andreluiskunert@gmail.com>
2026-04-13 00:58:43 -0300

    Bora La codar Com Java_2026

--- secao1/HelloWorld.java
+++ secao1/HelloWorld.java
@@ -1,7 +1,40 @@
-package secao1;
+package secao1; 
+// 
 
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Good evening my friends dev");
+
   }
 }
+//  Obs.:
+// desenvolvedor-full-stack@desenvolvedor-full-stack-ThinkPad-T440p:~/Home/Hora de Codar_2026/Java do básico ao avançado - com exercícios e projetos/LinguagemJava_2026/Java_do_basico_a
+// o_avan-ado_com_exercicios_e_projetos_2026/secao1$ javac .
+// ./  ../ 
+// desenvolvedor-full-stack@desenvolvedor-full-stack-ThinkPad-T440p:~/Home/Hora de Codar_2026/Java do básico ao avançado - com exercícios e projetos/LinguagemJava_2026/Java_do_basico_a
+// o_avan-ado_com_exercicios_e_projetos_2026/secao1$ javac .\HelloWorld.java
+// error: file not found: .HelloWorld.java
+// Usage: javac <options> <source files>
+// use --help for a list of possible options
+// desenvolvedor-full-stack@desenvolvedor-full-stack-ThinkPad-T440p:~/Home/Hora de Codar_2026/Java do básico ao avançado - com exercícios e projetos/LinguagemJava_2026/Java_do_basico_a
+// o_avan-ado_com_exercicios_e_projetos_2026/secao1$ javac .\HelloWorld.java
+// error: file not found: .HelloWorld.java
+// Usage: javac <options> <source files>
+// use --help for a list of possible options
+// desenvolvedor-full-stack@desenvolvedor-full-stack-ThinkPad-T440p:~/Home/Hora de Codar_2026/Java do básico ao avançado - com exercícios e projetos/LinguagemJava_2026/Java_do_basico_a
+// o_avan-ado_com_exercicios_e_projetos_2026/secao1$ javac HelloWorld.java
+// desenvolvedor-full-stack@desenvolvedor-full-stack-ThinkPad-T440p:~/Home/Hora de Codar_2026/Java do básico ao avançado - com exercícios e projetos/LinguagemJava_2026/Java_do_basico_a
+// o_avan-ado_com_exercicios_e_projetos_2026/secao1$ java HelloWorld
+// Erro: Não foi possível localizar nem carregar a classe principal HelloWorld
+// Causada por: java.lang.NoClassDefFoundError: HelloWorld (wrong name: secao1/HelloWorld)
+// desenvolvedor-full-stack@desenvolvedor-full-stack-ThinkPad-T440p:~/Home/Hora de Codar_2026/Java do básico ao avançado - com exercícios e projetos/LinguagemJava_2026/Java_do_basico_a
+// o_avan-ado_com_exercicios_e_projetos_2026/secao1$ java secao1.HelloWorld
+// Erro: Não foi possível localizar nem carregar a classe principal secao1.HelloWorld
+// Causada por: java.lang.ClassNotFoundException: secao1.HelloWorld
+// desenvolvedor-full-stack@desenvolvedor-full-stack-ThinkPad-T440p:~/Home/Hora de Codar_2026/Java do básico ao avançado - com exercícios e projetos/LinguagemJava_2026/Java_do_basico_a
+// o_avan-ado_com_exercicios_e_projetos_2026/secao1$ cd ..
+// desenvolvedor-full-stack@desenvolvedor-full-stack-ThinkPad-T440p:~/Home/Hora de Codar_2026/Java do básico ao avançado - com exercícios e projetos/LinguagemJava_2026/Java_do_basico_a
+// o_avan-ado_com_exercicios_e_projetos_2026$ java secao1.HelloWorld
+// Good evening my friends dev
+// desenvolvedor-full-stack@desenvolvedor-full-stack-ThinkPad-T440p:~/Home/Hora de Codar_2026/Java do básico ao avançado - com exercícios e projetos/LinguagemJava_2026/Java_do_basico_a
+// o_avan-ado_com_exercicios_e_projetos_2026$ 
