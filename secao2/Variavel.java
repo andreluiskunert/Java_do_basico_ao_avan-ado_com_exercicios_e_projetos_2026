575bfdf0c46f160fa562409498c7dc2bdc50f10e
Andre Luis Kunert <andreluiskunert@gmail.com>
2026-04-13 23:09:16 -0300

    bora la codar em Java_2026_hora_de_codar

---
+++ secao2/Variavel.java
@@ -0,0 +1,13 @@
+package secao2;
+
+public  class Variavel {
+   public static void main(String[] args) {
+    // 1-  O que são variáveis?
+    //  tipo -> nome -> atribuir um valor;
+    String nome = "Andre Luis";
+    // nome => "Andre Luis"
+    System.out.println("Meu nome é " + nome +";");
+
+   }
+    
+}
