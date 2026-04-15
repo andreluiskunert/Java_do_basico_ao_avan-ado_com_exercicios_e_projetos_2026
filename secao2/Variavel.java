5e0db22e70c153e31b02f7fc9279cbd377b154fa
Andre Luis Kunert <andreluiskunert@gmail.com>
2026-04-14 22:41:22 -0300

    bora la codar com Java_2026

--- secao2/Variavel.java
+++ secao2/Variavel.java
@@ -7,7 +7,17 @@ public  class Variavel {
     String nome = "Andre Luis";
     // nome => "Andre Luis"
     System.out.println("Meu nome é " + nome +";");
+   //  2ª Atribuição de valor com outra variável
+   String teste = "teste";
+   String testandosString =  teste;
+  System.out.println(testandosString);  
+  long numeroGrande = 999999999999l;
+  System.out.println(numeroGrande);
+//   7 -  long
+ long populacoaMundial = 77777777777l;
+ long grandNumero = 1_000_000l;
 
+System.out.println(grandNumero +1);
+System.out.println(populacoaMundial);
    }
-    
 }
