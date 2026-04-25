043d94cbf2e91e2fda427142900de9cb80e26c71
Andre Luis Kunert <andreluiskunert@gmail.com>
2026-04-25 11:37:24 -0300

    bora la codar com Java_2026

--- secao05/Condicionais.java
+++ secao05/Condicionais.java
@@ -22,73 +22,74 @@ public class Condicionais {
            // x é maior ou igual a 24
         System.out.println(x >=24 );
 
-        
-        // System.out.println(x < 10);
+        // Sabado,25/04/2026_De mada 
+        System.out.println("===Diferença entre comparação e atribuição===");
+        System.out.println(x < 25);
 
-        // // x é menor ou igual 10
-        // System.out.println(x <= 10);
+        // x é menor ou igual 25
+        System.out.println(x <= 25);
 
-        // // 3 - atribuição e comparação
-        // int n = 5;
-        // int m = 10;
+        // 3 - atribuição e comparação
+        int n = 5;
+        int m = 25;
 
-        // // System.out.println(n = 12);
+        // System.out.println(n = 12);
 
-        // System.out.println(n == 12);
+        System.out.println(n == 12);
 
-        // System.out.println(n);
+        System.out.println(n);
 
-        // System.out.println(n == m);
+        System.out.println(n == m);
 
-        // // 4 - Comparaçao de strings
+        System.out.println("==== Comparaçao de strings=====");
 
-        // String str1 = "Java";
-        // String str2 = new String("Java");
+        String str1 = "Java";
+        String str2 = new String("Java");
 
-        // System.out.println(str1);
-        // System.out.println(str2);
+        System.out.println(str1);
+        System.out.println(str2);
 
-        // System.out.println(str1 == str2);
+        System.out.println(str1 == str2);
 
-        // System.out.println(str1 == "Java");
+        System.out.println(str1 == "Java");
 
-        // System.out.println(str1.equals(str2));
+        System.out.println(str1.equals(str2));
 
-        // System.out.println(str2.equals(str1));
+        System.out.println(str2.equals(str1));
 
-        // System.out.println(str1.equals("Java"));
+        System.out.println(str1.equals("Java"));
 
-        // String str3 = "JAVA";
+        String str3 = "JAVA";
 
-        // System.out.println(str1.equals(str3));
+        System.out.println(str1.equals(str3));
 
-        // // Java == JAVA (false) > java == java
-        // System.out.println(str1.equalsIgnoreCase(str3));
-
-        // // 5 - if
-        // int numero = 10;
-
-        // // Baseado em uma comparação, eu executo algo
-        // if (numero > 125) {
-
-        //     System.out.println("O número é maior que 5");
-
-        // }
-
-        // // If com strings
-        // String texto = "Teste";
-
-        // // Se a função retorna um booleano, eu posso usar no if
-
-        // if(texto.equals("Teste")) {
-
-        //     System.out.println("O texto é: Teste");
-
-        // }
-
-        // // declaração do if (comparação ou retorno de booleano) { o que será executado... }
-
-
+        // Java == JAVA (false) > java == java
+        System.out.println(str1.equalsIgnoreCase(str3));
+        System.out.println("===//====///====//============///============///===========///=====");
+        System.out.println("Estruturas de condição: ");
+        System.out.println(
+                        "●if: Executa um bloco de código se a condição for verdadeira;\n" + //
+                        "●else: Executa um bloco de código alternativo se a condição do if for falsa;\n" + //
+                        "●else if: Verifica outra condição se as condições anteriores forem falsas;\n" + //
+                        "●switch: Seleciona e executa um bloco de código entre várias opções com base no\n" + //
+                        "valor de uma expressão;\n" + //
+                        "●\n" + //
+                        "Importante: Todas essas estruturas permitem controlar o fluxo de execução com base\n" + //
+                        "em condições lógicas;"
+                         );
+        System.out.println("§ if :");
+         int numero = 25;
+         if (numero > 155) {
+            System.out.println("O numero é maior que 5");
+            
+         }
+         System.out.println(" If com strings");
+         String texto = "Good Morning";
+         if (texto.equals(texto)) {
+             System.out.println("O texto é : " + texto);
+         }
+         System.out.println("obs.:   declaração do if (comparação ou retorno de booleano) { o que será executado... }");
+         
         // // 6 - else
         // int q = 17;
 
