4dfc02be3b97bb63f24a05be246c594d36beb889
Andre Luis Kunert <andreluiskunert@gmail.com>
2026-05-02 15:40:40 -0300

    Bora codar com Java_REcordando 2020/2026

---
+++ secao08/funcoes.java
@@ -0,0 +1,13 @@
+package secao08;
+
+public class funcoes {
+    public static void main(String[] args) {
+        // Criando a primeira função_Sábado, 02/05/2026_De Tarde
+        System.out.println("Criando a primeira função:"); 
+        saudacao();
+    }
+   public static void saudacao(){
+      System.out.println("ola... essa minha 1ª função");
+    }
+
+}
