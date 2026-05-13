56c4e66a9561e0b9e566f2817d4dacf144ac8aec
Andre Luis Kunert <andreluiskunert@gmail.com>
2026-05-12 22:34:05 -0300

    Bora codar com Java, 2020/2026

--- secao11/loops.java
+++ secao11/loops.java
@@ -36,6 +36,8 @@ public class loops {
         }
 
         // 2 - While
+        // Terça-feira,12/05/2026_De Noite
+        System.out.println("Terça-feira,12/05/2026_De Noite {While } ");
         int i = 0;
 
         while(i <= 5) {
@@ -48,6 +50,38 @@ public class loops {
             // 
 
             i++;
-        }
-        }
+           
+          } 
+        //   int valor = 0;
+        //   while (valor != 7) {
+        //     valor = ((int)Math.random() * 10);
+        //     // System.out.println("valor aleatorio: " + valor + ";");
+            
+        //   }
+        // loop infinito
+        // System.out.println("loop infinito");
+        // while (true) {
+        //     System.out.println("Ao Infinito é além...");
+        // }
+        // for(int j = 0; j <= 5; j--){
+        //     System.out.println(j);
+        // }
+        System.out.println("Do while");
+        int j = 10;
+        do{
+        System.out.println("O valor de j é :" + 1);
+         j--;
+        } while(j > 0);
+         int numero = 0;
+
+        do {
+
+            numero = (int)(Math.random() * 10);
+
+            System.out.println("numero aleatorio: " + numero);
+
+        } while (numero != 1);
+
+
+     }
 }
