95ae2b6dd382ab4b265f0dde4348b4a9d9952878
Andre Luis Kunert <andreluiskunert@gmail.com>
2026-05-19 22:22:31 -0300

    bora la codar com Java_2020/2026

--- secao14/Listas.java
+++ secao14/Listas.java
@@ -4,7 +4,7 @@ public class Listas {
     public static void main(String[] args) {
         System.out.println("tipos de Arrays: ");
         // 1 - Sintaxe:
-        int[] numeros = {0, 1, 2, 3, 4, 5, 6, 7, 8};
+        int[] numeros = {1, 2, 3, 4, 5, 6, 7, 8};
         // {elem1, elem2 elemX...}
         System.out.println("Acessando o 1ª elemento: " + numeros[0]);
         //  tamanho tixo, só que vázio
@@ -19,6 +19,42 @@ public class Listas {
         double[] precos = {1.99, 2.45, 4.97};
         precos[2] = 5.00;
         System.out.println(precos[2]);
+    //   Terça-feira,19/05/2026_De noite
+    System.out.println("====///=====///=====///=====///=====///===");
+    System.out.println("2_Loops em Arrays");
+    int soma = 0;
+    for(int i = 0; i < numeros.length; i++){
+    // i > 0; i > 1; i > 2...
+    soma += numeros[1];
+
+    }
+    System.out.println("Soma dos Elementos: " + soma + ";");
+ // for each
+        // Nomear o item => Array
+        // Pessoas => Pessoa
+        // Frutas => Fruta
+        for(String fruta : frutas) {
+            System.out.println("A fruta da vez: " + fruta);
+        }
+
+        // encontrar o maior valor de um array
+        int[] valores = {17, 27, 877, 77, 1};
+
+        int maiorValor = valores[0];
+
+        int j = 0;
+
+        while(j < valores.length) {
+
+            if(valores[j] > maiorValor) {
+                maiorValor = valores[j];
+            }
+
+            j++;
+
+        }
+
+        System.out.println("O maior valor é: " + maiorValor);
 
     }
 }
