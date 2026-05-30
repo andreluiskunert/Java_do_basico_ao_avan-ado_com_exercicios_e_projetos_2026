2596673c67576f599f7a234beaccabab36482695
Andre Luis Kunert <andreluiskunert@gmail.com>
2026-05-29 22:15:29 -0300

    bora codar Java_2020/2026

---
+++ secao17/Carro.java
@@ -0,0 +1,16 @@
+ // package secao_17;
+
+public class Carro {
+    // atributos ou propriedades 
+    String marca;
+    String modelo;
+    int ano; // 2012
+    // Métodos
+     void acelerar(){
+        System.out.println("Pisando fundo lá no fim da lata...");
+     }
+     public void exibirInfo(){
+      System.out.println("Marca:" + marca + " Modelo:" + modelo + "ano:" + ano +";" );
+     }
+
+}
