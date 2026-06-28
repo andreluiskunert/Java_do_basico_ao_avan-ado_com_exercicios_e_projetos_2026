50823b91a05293fa905f5b24022bd130ceb01fc5
Andre Luis Kunert <andreluiskunert@gmail.com>
2026-06-27 23:54:49 -0300

    Codando em Java

--- secao21/Erros.java
+++ secao21/Erros.java
@@ -1,10 +1,11 @@
 package secao21;
 // Quinta-feira, 25/062026_D'noite
+import java.io.*;//Sábado,27/06/2026_d'noite
 public class Erros {
    
     public static void main(String[] args) {
         // try catch
-        System.out.println("try catch");
+        System.out.println("01_try catch");
         try{
             int a = 0;
             int b = 10;
@@ -20,7 +21,7 @@ public class Erros {
         }
         System.out.println("===//=====//========//=====");
         // Sexta-feira,26/06/2026_d'noite
-        System.out.println("Bloco finaly");
+        System.out.println("02_Bloco finaly");
         try {
             int[] numeros = {1,2,3};
             System.out.println(numeros[3]);
@@ -31,6 +32,51 @@ public class Erros {
         } finally{
             System.out.println("Executou o finaly");
         }
+        System.out.println("===//=====//========//=====");
+        // Sábado,27/06/2026_d'noite
+        System.out.println("03_verificadas e nao verificadas:");
+        // 
+        System.out.println("03_01Verificada:");
+          try {
+            BufferedReader reader = new BufferedReader(new FileReader("arquivo.txt"));
+            String linha = reader.readLine();
+            System.out.println(linha);
+        } catch (Exception e) {
+            System.out.println("Erro ao ler arquivo: " + e.getMessage());
+        }
+     System.out.println("03_02Não_verificada:");
+      // não verificadas
+        String texto = null;
+
+        // System.out.println(texto.length());
+          System.out.println("===//=====//========//=====");
+        // Sábado,27/06/2026_d'noite
+        System.out.println("04_exceções_com_throw:");
+ try {   
+
+            validarIdade(20);
+            
+            validarIdade(10);
+
+        } catch (Exception e) {
+            System.out.println("Erro: " + e.getMessage());
+        }
+              System.out.println("===//=====//========//=====");
+        // Sábado,27/06/2026_d'noite
+        System.out.println("05_exceções_customizadas:");
+        System.out.println("Continue....");
+        System.out.println("===//=====//========//=====");
+
   System.out.println("       The End       ");
     }
+     public static void validarIdade(int idade) {
+
+        if(idade < 18) {
+            throw new IllegalArgumentException("Idade deve ser maior que 18.");
+        }
+
+        System.out.println("Idade válida :" + idade);
+
+    }
+
 }
