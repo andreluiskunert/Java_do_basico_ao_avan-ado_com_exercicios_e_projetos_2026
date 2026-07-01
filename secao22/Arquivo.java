dd54b369469f7a3bd6dfa640f8823c001f4fdfae
Andre Luis Kunert <andreluiskunert@gmail.com>
2026-06-30 22:36:03 -0300

    Bora La Codar com Java_2026

---
+++ secao22/Arquivo.java
@@ -0,0 +1,13 @@
+package secao22;
+import java.io.*;
+public class Arquivo {
+    public static void main(String[] args) {
+        System.out.println("01_Leitura de Arquivo:");
+        //   Java_do_basico_ao_avan-ado_com_exercicios_e_projetos_2026/secao22/Arquivo.java
+        // Obs.: Caminho Relativo
+        String currentDir = System.getProperty("user.dir");
+   System.out.println("Resultado 01 é :" + currentDir + " arquivo.txt ;");
+   System.out.println("Resultado 02"+"Java_do_basico_ao_avan-ado_com_exercicios_e_projetos_2026/secao22");
+        System.out.println("The End...");
+    }
+}
