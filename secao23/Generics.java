8c88b94482ab4183f64747dc37cdbde6a54b1611
Andre Luis Kunert <andreluiskunert@gmail.com>
2026-07-17 23:10:39 -0300

    bora la codar com Java_2026

--- secao23/Generics.java
+++ secao23/Generics.java
@@ -52,9 +52,38 @@ Comparador<Integer> comparadorInteiros = new Comparador<>();
     
         // Erro de compilação
         // Comparador<String> comparadorStrings = new Comparador<>();
+         System.out.println("===//===//===//======//======//=======//====");
+          System.out.println("04_wildcard:");
+          // Sexta-feira_D'noite, 17/07/2026
+             List<Integer> numeros2 = List.of(1, 2, 3);
+        List<String> palavras2 = List.of("teste", "Java", "olá");
+
+        // aceita qualquer tipo de lista (?)
+        imprimirLista(numeros2);
+
+        imprimirLista(palavras2);
+
+        // aceita listas numericas (? extends Number)
+        System.out.println(somarNumeros(numeros2));
+
+        // somarNumeros(palavras2);
+
+        // aceita apenas tipos numericos especificos
+        // Number > Integer (5,4,3)
+        List<Integer> numeros3 = new ArrayList<>();
+
+        // adicionarNumeros aceita apenas Lista de Inteiros
+        adicionarNumeros(numeros3);
+
+        for(Integer numero : numeros3) {
+            System.out.println(numero);
+        }
+
+        
           System.out.println("----///-----//-----");
         System.out.println(" The End...");
     }
+   
     public static <T extends Comparable<T>> T obterMaior(T valor1, T valor2){
       return (valor1.compareTo(valor2) > 0) ? valor1 : valor2;
 
@@ -64,4 +93,27 @@ Comparador<Integer> comparadorInteiros = new Comparador<>();
         System.out.println(elemento + " , ");
       }
     }
-}
+    public static void imprimirLista(List<?> lista){
+      for(Object elemento : lista) {
+            System.out.println(elemento);
+        }
+    }
+    public static double somarNumeros( List<? extends Number> lista ){
+     double soma = 0;
+
+        for(Number numero : lista) {
+            soma += numero.doubleValue();
+        }
+
+        return soma;
+
+    }
+    public static void adicionarNumeros(List<? super Integer> lista){
+      for(int i = 1; i <= 5; i++) {
+            lista.add(i);
+        }
+
+    }
+
+    }
+
