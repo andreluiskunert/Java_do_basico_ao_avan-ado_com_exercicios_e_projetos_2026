a48f1acd54e4edcd22ec6949107d746bdebe2fe5
Andre Luis Kunert <andreluiskunert@gmail.com>
2026-09-07 22:34:04 -0300

    bora la codar com JAva_2026

---
+++ secao27/HelloWorldFx/src/hellofx/CounterModel.java
@@ -0,0 +1,16 @@
+package hellofx;
+// Segunda-feira_Feriado,07/09/2026_D'noite
+public class CounterModel {
+    private  int count;
+    public int getCount(){
+        return count;
+    }
+    public void  increment(){
+       count++;
+    }
+    public void reset(){
+        count = 0;
+        
+    }
+    
+}
